f08eee2e9eed5c8acd0c2f3aa78d371307f9f879
mazurenkooleksandr <kakchtoinfo@ukr.net>
2021-02-04 17:11:13 +0200

    cycles-fourth-task-done

---
+++ js/cycles-fourth.js
@@ -0,0 +1,6 @@
+function fourthFun() {
+    let number = document.getElementById('cycles-fourth-number').value;
+    let resultNumber = document.getElementById("cycles-fourth-result");
+        resultNumber.innerHTML = 'Кількість цифр: ' + number.length;
+}
+
--- js/cycles-third.js
+++ js/cycles-third.js
@@ -2,9 +2,6 @@ function thirdFun() {
     let number = document.getElementById('cycles-third-number').value;
     let arr = [];
     let resultNumber = document.getElementById("cycles-third-result");
-    if(number < 0){
-        number = number * -1;
-    }
     for (let i = 1; i <= number; i++) {
               
         if (number % i == 0) {
