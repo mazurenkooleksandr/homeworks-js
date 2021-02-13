82e98b33b455bbc236d5ab78eee018b6dde611dd
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-02-13 11:05:54 +0200

    Numbers way example

--- js/function-third.js
+++ js/function-third.js
@@ -4,15 +4,22 @@ document.querySelector('.function-third-button').onclick = function(){
     let a = document.querySelector('.function-third-a-input').value;
     let b = document.querySelector('.function-third-b-input').value;
     let c = document.querySelector('.function-third-c-input').value;
-    let newArray = [];
 
-    if(a != '' && b != '' && c != ''){
-        newArray.push([a]);
-        newArray.push([b]);
-        newArray.push([c]);
-    } else {
-        alert('Введіть число');
+    a = Number.parseInt(a);
+    b = Number.parseInt(b);
+    c = Number.parseInt(c);
+
+    if (isNaN(a) || isNaN(b) || isNaN(c)) {
+      alert("Введите цифры правильно");
+      return;
     }
     
-    text.textContent = `Результат: ${newArray.join('')}`;
+    if ((a < 1 || a > 9) || (b < 0 || b > 9) || (c < 0 || c > 9)) {
+      alert("Введите цифры правильно");
+      return;
+    }
+
+    let result = a * 100 + b * 10 + c;
+    
+    text.textContent = `Результат: ${result}`;
 }
